a07888d1b775c5f703ac3db301e3be9fd3390f32
tracer-ics2017 <tracer@njuics.org>
2019-04-27 15:58:59 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:58:59 up 13:18,  1 user,  load average: 0.28, 0.12, 0.03 376fa4c711e964c29be442259b5ccf24be666ddf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -246,8 +246,8 @@ uint32_t expr(char *e, bool *success) {
     return 0;
   }
 
-  for(int i=0;i<nr_token;i++)  //负号的判断 当其为第一个符号，或左边为(时
-      if(tokens[i].type == '-' &&(i==0||tokens[i-1].type == '('||tokens[i-1].type == '-'))
+  for(int i=0;i<nr_token;i++)  //负号的判断 当其为第一个符号，或左边为(时,或按照讲义左边可能为负号(--1)
+      if(tokens[i].type == '-' &&(i==0||tokens[i-1].type == '('||tokens[i-1].type == TK_NEG))
           tokens[i].type = TK_NEG;
   //*success = true;
   /* TODO: Insert codes to evaluate the expression. */
