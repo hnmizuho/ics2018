8db3340d0cb4130ec74c48192a9c197d6649fdc3
tracer-ics2017 <tracer@njuics.org>
2019-04-27 14:08:13 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:08:13 up 11:27,  1 user,  load average: 0.21, 0.09, 0.02 c7d55d48bb9961349746957b6f9d8bd2ee5613f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -238,8 +238,8 @@ uint32_t eval(int p,int q){
             }
         }
 
-        uint32_t val1 = eval(p,op-1);printf("%d",val1);
-        uint32_t val2 = eval(op+1,q);printf("\n%d",val2);
+        uint32_t val1 = eval(p,op-1);printf("%d\n",val1);
+        uint32_t val2 = eval(op+1,q);printf("%d\n",val2);
         switch(op_type){
             case '+':return val1+val2;
             case '-':return val1-val2;
