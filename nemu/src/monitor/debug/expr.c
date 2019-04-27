c1f3686e58b1cfaa14e56a9ebc6064cfebd3094d
tracer-ics2017 <tracer@njuics.org>
2019-04-27 16:30:52 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:30:52 up 13:50,  1 user,  load average: 0.03, 0.09, 0.06 85a2432de572734f709e68b4bf5041da94a9ca37

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -163,16 +163,13 @@ bool check_parentheses(int p,int q){
 uint32_t eval(int p,int q){
     if(p>q){   //单目运算符时会出现此情况，如3+-1解释为3+ 和-1，3+缺省为3+0 
         // printf("Bad expression\n");
-        if(tokens[p].type == TK_NEG)
-            return -1; //p -1 return -1*1
         return 0;
     }
     else if(p==q){
         uint32_t res;
         if(tokens[p].type == TK_HEX) sscanf(tokens[p].str,"%x",&res);
         else if(tokens[p].type == TK_DEC) sscanf(tokens[p].str,"%d",&res);
-        else if(tokens[p].type == TK_NEG)return 1; //p --1 return 1*1
-        else return 0;
+        else assert(0);
         return res;
     }
     else if(check_parentheses(p,q) == true){
