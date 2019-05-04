8294e2dd8c8b59bf23615728d57158a1eeb68272
tracer-ics2017 <tracer@njuics.org>
2019-05-04 22:30:30 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:30:30 up  8:28,  1 user,  load average: 0.12, 0.13, 0.11 ed0cb051df2571af6f4f3da8886ce4a9e0d615

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -21,6 +21,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_S:
     case CC_L:
     case CC_LE:
+
+    case CC_NE:*dest = cpu.eflags.ZF != 1 ? 1 : 0;break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
