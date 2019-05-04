76654365d6514a1149e2f33936b9951d7d40478a
tracer-ics2017 <tracer@njuics.org>
2019-05-04 20:56:45 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:56:45 up  6:54,  1 user,  load average: 0.00, 0.01, 0.01 4068b55b37eadfd9b85311cd441358b16aae52f

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -6,7 +6,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 10
+#define MAX_INSTR_TO_PRINT 100
 
 int nemu_state = NEMU_STOP;
 
