f85d92533e792e1cd48371d8ed5c166c1db707f8
tracer-ics2017 <tracer@njuics.org>
2019-05-05 20:48:43 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:48:43 up 21:34,  1 user,  load average: 0.11, 0.19, 0.30 4a02890f2e4ae864ad756285426d1b86d73c5c5

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,9 +14,12 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
+    case CC_O:*dest = cpu.eflags.OF == 1 ? 1 : 0;break;
+    case CC_NO:*dest = cpu.eflags.OF != 1 ? 1 : 0;break;
+    case CC_B: *dest = cpu.eflags.ZF == 1 ? 1 : 0;break;
+    case CC_NB:*dest = cpu.eflags.ZF == 0 ? 1 : 0;break;
     case CC_E:*dest = cpu.eflags.ZF == 1 ? 1 : 0;break;
+    case CC_NE:*dest = cpu.eflags.ZF != 1 ? 1 : 0;break;
     case CC_BE:*dest = (cpu.eflags.CF == 1 || cpu.eflags.ZF == 1) ? 1 : 0;break;
     case CC_NBE:*dest = (cpu.eflags.CF != 1 && cpu.eflags.ZF != 1) ? 1 : 0;break;
     case CC_S:*dest = cpu.eflags.SF == 1 ? 1 : 0;break;
@@ -28,7 +31,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_LE:*dest = ((cpu.eflags.ZF == 1)||(cpu.eflags.SF != cpu.eflags.OF)) ? 1 : 0;break;
     case CC_NLE:*dest = ((cpu.eflags.ZF != 1)&&(cpu.eflags.SF == cpu.eflags.OF)) ? 1 : 0;break;
 
-    case CC_NE:*dest = cpu.eflags.ZF != 1 ? 1 : 0;break;
+
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
