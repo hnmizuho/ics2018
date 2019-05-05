bffa3702b1dbc51ac038dd4c6c72bbe7f4f22b1d
tracer-ics2017 <tracer@njuics.org>
2019-05-05 14:49:49 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:49:49 up 15:35,  1 user,  load average: 0.72, 0.22, 0.13 a1ea670a3c16ac3738b7c19c12c2cb9ed7ca7146

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -12,6 +12,8 @@ make_EHelper(test) {
 make_EHelper(and) {
   rtl_and(&t0,&id_dest->val,&id_src->val);
   operand_write(id_dest,&t0);
+
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
 
