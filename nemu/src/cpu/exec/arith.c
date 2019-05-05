6c974eafb706c0a44a85652d2009170c59b7d7d4
tracer-ics2017 <tracer@njuics.org>
2019-05-05 09:50:52 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  09:50:52 up 11:13,  1 user,  load average: 0.34, 0.14, 0.10 d411d3db535abd24ff31a296786e636768de3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -48,14 +48,13 @@ make_EHelper(cmp) {
   //cmp不存结果，只改变cflags
   //但似乎存了也没问题，反正不会用到 直接存到src里作为中间量
   //复用sub代码，但是src2需要符号扩展
-  rtl_sext(&id_dest->val,&id_dest->val,id_dest->width);
-  rtl_sub(&t2, &id_src->val, &id_dest->val);
-  rtl_sltu(&t3, &id_src->val, &t2);
-printf("t2 %08x\n",t2);
+  rtl_sext(&id_src->val,&id_src->val,id_src->width);
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
+  rtl_sltu(&t3, &id_dest->val, &t2);
+
   //operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
-rtl_get_SF(&t0);
-printf("SF %08x\n",t0);
+
   //rtl_sltu(&t0, &id_dest->val, &t2);
   //rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t3); //before t0 after t3
