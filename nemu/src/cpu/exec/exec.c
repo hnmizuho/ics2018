f53782166ac0be761d299e90dfb1a660ccf25536
tracer-ics2017 <tracer@njuics.org>
2019-05-05 13:52:19 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  13:52:19 up 14:38,  1 user,  load average: 0.08, 0.10, 0.07 28698cf0c19f567a216cf16748f8c632cd0c01b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -52,7 +52,7 @@ make_group(gp2,
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
-    EX(test), EMPTY, EX(not), EMPTY,
+    IDEX(test_I,test), EMPTY, EX(not), EMPTY,
     EX(mul), EX(imul1), EX(div), EX(idiv))
 
   /* 0xfe */
