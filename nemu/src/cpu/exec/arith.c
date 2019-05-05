d9d0ef914b3cfe316eb9a9a7e528d598f96c687f
tracer-ics2017 <tracer@njuics.org>
2019-05-05 14:01:56 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:01:56 up 14:47,  1 user,  load average: 0.08, 0.18, 0.14 c78e433e7e77d757b113b31ab14f1bb3f03893f5

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -57,7 +57,7 @@ make_EHelper(cmp) {
 
   //rtl_sltu(&t0, &id_dest->val, &t2);
   //rtl_or(&t0, &t3, &t0);
-  rtl_set_CF(&t0);
+  rtl_set_CF(&t3); //before:t0
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
   rtl_xor(&t1, &id_dest->val, &t2);
