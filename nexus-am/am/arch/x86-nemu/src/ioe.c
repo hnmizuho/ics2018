b3fe12d7fa5a902b77bc7d114ebd208340e09bf7
tracer-ics2017 <tracer@njuics.org>
2019-05-06 22:33:35 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:33:35 up 1 day,  2:42,  1 user,  load average: 0.22, 0.23, 0.16 1fcf394f5b8658db4f50d61cdaf571389c8e27b9

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -33,5 +33,9 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+    uint32_t code = _KEY_NONE;
+    if(inb(0x64) & 0x1)
+        code = inl(0x60);
+    // return _KEY_NONE;
+    return code;
 }
