12b62bd05c9985b98503a7be43c17e7210bb0193
tracer-ics2017 <tracer@njuics.org>
2019-05-07 14:21:25 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:21:25 up  3:26,  1 user,  load average: 0.22, 0.37, 0.64 7a9d39dd3bcaa9104fdb2b009aae0ca8966519f1

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,11 +1,11 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG
-#define DIFF_TEST
+//#define DEBUG
+//#define DIFF_TEST
 
 /* You will define this macro in PA2 */
-//#define HAS_IOE
+#define HAS_IOE
 
 #include "debug.h"
 #include "macro.h"
