5db0cbd03dd065180d491fb5f91eee8aec7644db
tracer-ics2017 <tracer@njuics.org>
2019-05-07 15:23:52 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:23:52 up  4:29,  1 user,  load average: 0.09, 0.33, 0.37 5265278091d2fc8cc41595f6acd2e14ae20132b8

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -125,7 +125,7 @@ make_rtl_setget_eflags(OF)
 make_rtl_setget_eflags(ZF)
 make_rtl_setget_eflags(SF)
 
- void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
+static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
   *dest = *src1;
 }
