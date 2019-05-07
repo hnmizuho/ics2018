06a8e9388f9ab054af515b03ed90edfbd3ad556e
tracer-ics2017 <tracer@njuics.org>
2019-05-07 14:46:53 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:46:53 up  3:52,  1 user,  load average: 0.40, 0.22, 0.35 e3f58771b244de310a802dd8b80fa97a4b8764c

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,14 +23,11 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  // int i;
-  //for (i = 0; i < _screen.width * _screen.height; i++) {
-  //  fb[i] = i;
-  //}
-  int row,col;
-  for(row = y;row<y+h;row++)
-      for(col = x;col<x+w;col++)
-          fb[col+row*_screen.width] = pixels[(row-y)*col+col-x];
+   int i;
+   for (i = 0; i < _screen.width * _screen.height; i++) {
+    fb[i] = i;
+   }
+  // pixels指定的像素颜色 填充到（x，y）到（x+w，y+h)上
   /*int i;
   for(i=0;i<h;i++)
       memcpy(fb+(y+i)*_screen.width+x,pixels+i*w,w*4);*/
