bd3e08858eb83bc63bc9b90472455544276fd4ae
tracer-ics2017 <tracer@njuics.org>
2019-05-08 22:28:37 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:28:37 up  4:15,  1 user,  load average: 0.06, 0.08, 0.13 76abbbad97fa6ab7e805f8e5b87e7f49ea92bcad

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -16,7 +16,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   
   //cpu.cs = ?? //nemu不必设置
   rtl_li(&t0,vaddr_read(cpu.idtr.i_base+4*NO,4));
-  rtl_li(&t1,vaddr_read(cpu.idtr.i_base+4*NO+2,4));
+  rtl_li(&t1,vaddr_read(cpu.idtr.i_base+4*NO+2,2));
   //if((t1 & 0x00008000) == 0)
       //assert(0);
 
