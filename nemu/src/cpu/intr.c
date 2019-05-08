5065dabd57447413ae46548635a79d139f24b3bf
tracer-ics2017 <tracer@njuics.org>
2019-05-08 22:11:01 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:11:01 up  3:57,  1 user,  load average: 0.20, 0.33, 0.26 24c37ccb158509d3b52dfe954229aea02ebe104f

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -18,10 +18,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   
   //cpu.cs = ??
   rtl_li(&t0,vaddr_read(cpu.idtr.i_base+4*NO,4));
+  rtl_li(&t1,vaddr_read(cpu.idtr.i_base+4*NO+4,4));
   //if((t0 & 0x00008000) == 0)
   //    assert(0);
   //cpu.eip = ret_addr + (t0 & 0x00001111);
-  decoding.jmp_eip = t0&0xffff;
+  decoding.jmp_eip = (t0&0xffff)|(t1&0xffff0000);
   //rtl_addi(&decoding.jmp_eip,&ret_addr,t0);
   decoding.is_jmp = 1;
 }
