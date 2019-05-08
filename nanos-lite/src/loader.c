df772b520b5f9486784ac0317a348cd4c7dd0bfa
tracer-ics2017 <tracer@njuics.org>
2019-05-08 19:30:35 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:30:35 up  1:17,  1 user,  load average: 0.29, 0.16, 0.07 a96d5124f30f2d6a3e5534eedf1415294fbbbf2

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,10 +4,9 @@
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
-  size_t size;
-  size = get_ramdisk_size();
+  size_t size = get_ramdisk_size();
   void * buff = NULL;
-  ramdisk_read(buff,0,size);
-  memcpy(DEFAULT_ENTRY,buff,size);
+  ramdisk_read(buff,0,size); 
+  memcpy(DEFAULT_ENTRY,buff,size); //之前误用memset
   return (uintptr_t)DEFAULT_ENTRY;
 }
