2254d7fed7db42918d3b95fb24542ac15e57298f
tracer-ics2017 <tracer@njuics.org>
2019-05-08 21:37:50 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:37:50 up  3:24,  1 user,  load average: 0.42, 0.17, 0.08 eb598f91771656175a6055ca6cea20386b3ed180

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -27,7 +27,8 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  raise_intr(id_dest->val,cpu.eip);
+  //raise_intr(id_dest->val,cpu.eip);
+  raise_intr(id_dest->addr,cpu.eip);
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
