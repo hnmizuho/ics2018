cda1edac0d58f802a34290ffbacff01922ff19a5
tracer-ics2017 <tracer@njuics.org>
2019-05-09 16:08:09 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:08:09 up  8:26,  1 user,  load average: 0.20, 0.16, 0.15 91bf0217f55f3eb6ab0f6efb939234035da9143

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -20,9 +20,9 @@ struct _RegSet {
 };
 
 #define SYSCALL_ARG1(r) r->eax
-#define SYSCALL_ARG2(r) r->ecx
-#define SYSCALL_ARG3(r) r->edx
-#define SYSCALL_ARG4(r) r->ebx
+#define SYSCALL_ARG2(r) r->ebx
+#define SYSCALL_ARG3(r) r->ecx
+#define SYSCALL_ARG4(r) r->edx
 
 #ifdef __cplusplus
 extern "C" {
