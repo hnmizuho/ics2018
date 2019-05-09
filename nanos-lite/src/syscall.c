94c0cd2454f8f73a6e1276cc95c18e46c7777314
tracer-ics2017 <tracer@njuics.org>
2019-05-09 15:46:37 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:46:37 up  8:04,  1 user,  load average: 0.67, 0.30, 0.17 49620c592da207e26cf461ef6973abc772795ff3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[0] = SYSCALL_ARG1(r);
 
   switch (a[0]) {
-    case SYS_none:return NULL;
+    case SYS_none:SYSCALL_ARG1(r) = 1;return NULL;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
