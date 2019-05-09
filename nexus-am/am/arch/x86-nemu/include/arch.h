c22e953c137b353fb2836153101546d281fdad42
tracer-ics2017 <tracer@njuics.org>
2019-05-09 14:54:25 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:54:25 up  7:12,  1 user,  load average: 0.00, 0.00, 0.03 e9d5be0fb922a0e7a37585ef2b823028ba379fb

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -16,8 +16,7 @@ struct _RegSet {
   uintptr_t eax,ecx,edx,ebx,esp,ebp,esi,edi;*/
   uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax;
   int       irq;
-  uintptr_t error_code;
-  uintptr_t eip,cs,eflags;
+  uintptr_t error_code,eip,cs,eflags;
 };
 
 #define SYSCALL_ARG1(r) 0
