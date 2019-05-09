2b7fe11c123a8d752e37176eceebccfdfd669dee
tracer-ics2017 <tracer@njuics.org>
2019-05-09 20:12:53 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:12:53 up 10:23,  1 user,  load average: 0.21, 0.29, 0.23 d5c9ffa62d2319e9ea9af8b2a2f7c4ce7fc1f44

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -26,7 +26,10 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
-  _exit(SYS_write);
+  //模仿上面给出来的_exit 
+  //_syscall_ 四个参数就是SYSCALL_ARGX！
+  _syscall_(SYS_write, fd, (uintptr_t)buf, count);
+  //_exit(SYS_write);
 }
 
 void *_sbrk(intptr_t increment){
