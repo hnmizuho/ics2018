e6ac47ca9fbee066b9c8bd907d9ad3d099ba69e9
tracer-ics2017 <tracer@njuics.org>
2019-05-09 20:18:54 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:18:54 up 10:29,  1 user,  load average: 0.24, 0.29, 0.25 933890d264fb656114204385993f9946781426e2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ static inline _RegSet* sys_exit(_RegSet *r){
   return NULL;
 }
 static inline _RegSet* sys_write(_RegSet *r){
-  /*int fd = (int)SYSCALL_ARG2(r);
+  int fd = (int)SYSCALL_ARG2(r);
   char *buf = (char *)SYSCALL_ARG3(r);
   int len = (int)SYSCALL_ARG4(r);
 
@@ -21,14 +21,7 @@ static inline _RegSet* sys_write(_RegSet *r){
       }
       SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
   }
-  return NULL;*/
-	if(SYSCALL_ARG2(r) == 1 || SYSCALL_ARG2(r) == 2){
-		for(int i = 0; i < SYSCALL_ARG4(r); i++) {
-			_putc(((char*)SYSCALL_ARG3(r))[i]);
-		}
-		SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
-	}
-	return NULL;
+  return NULL;
 }
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
