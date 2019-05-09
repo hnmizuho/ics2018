d15f60001b0af18f94b0e04e58c90fc3b5d04b3c
tracer-ics2017 <tracer@njuics.org>
2019-05-09 20:16:15 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:16:15 up 10:26,  1 user,  load average: 0.53, 0.32, 0.24 2e019d9cd30db85d3adb79c456a8f0e3edff7378

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,7 +33,7 @@ static inline _RegSet* sys_write(_RegSet *r){
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-printf("s");
+printf("%d",a[0]);
   switch (a[0]) {
     case SYS_none:return sys_none(r);
     case SYS_exit:return sys_exit(r);
