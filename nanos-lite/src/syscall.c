01b3767db0c9413cfa5691d71eb683abf38fbb5d
tracer-ics2017 <tracer@njuics.org>
2019-05-09 20:07:38 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:07:38 up 10:17,  1 user,  load average: 0.64, 0.32, 0.21 9fbdd85ea7baaea17d989271a82de2ee925fba3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,21 @@ static inline _RegSet* sys_none(_RegSet *r){
   return NULL;
 }
 static inline _RegSet* sys_exit(_RegSet *r){
+  //接受一个退出状态的参数，顺序ebx ecx edx
   _halt(SYSCALL_ARG2(r)); 
+  return NULL;
+}
+static inline _RegSet* sys_write(_RegSet *r){
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+
+  if(fd == 1 || fd == 2){
+      for(int i = 0; i < len; i++) {
+          _putc(buf[i]);
+      }
+      SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+  }
   return NULL;
 }
 _RegSet* do_syscall(_RegSet *r) {
