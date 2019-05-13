1ada1c53423431b8528634d6e001e0e38cb29904
tracer-ics2017 <tracer@njuics.org>
2019-05-13 22:45:04 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:45:04 up 23:09,  1 user,  load average: 0.17, 0.20, 0.18 1284101031ac98656cea90e7231fb7d6d71a0f7a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,7 +9,23 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  //return 0;
+	int key = _read_key();
+	bool down = false;
+	Log("key = %d\n", key);
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = true;
+	}
+	if (key == _KEY_NONE) {
+		unsigned long t = _uptime();
+		sprintf(buf, "t %d\n", t);
+	}
+	else {
+		//Log("I am here~\n");
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+	}
+	return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
