72a539d745eacfdd76ebbfba933d3e74166ad33f
tracer-ics2017 <tracer@njuics.org>
2019-05-13 22:05:31 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:05:31 up 22:29,  1 user,  load average: 0.17, 0.12, 0.18 6106cee41f541793d558175f99e4e5dcf4cd278

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-#define DEFAULT_ENTRY ((void *)0x8048000)
+#define DEFAULT_ENTRY ((void *)0x4000000)
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
