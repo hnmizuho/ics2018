1932fd41630669957c0555af1f1dc5426a5670cc
tracer-ics2017 <tracer@njuics.org>
2019-05-13 21:50:56 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:50:56 up 22:15,  1 user,  load average: 0.17, 0.23, 0.29 f9f7bea062d3b4a4c1423ac227b58f504e01773d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,7 +80,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
 				return 0;
 			if(file_table[fd].open_offset + len > fs_size)
 				len = fs_size - file_table[fd].open_offset;
-  Log("------------------------");
+  Log("%d------------------------",fd);
 			ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 			file_table[fd].open_offset += len;
 			break;
