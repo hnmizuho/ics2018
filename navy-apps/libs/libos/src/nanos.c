dcbc59a669523105983eedd730ec6cc576c20aff
tracer-ics2017 <tracer@njuics.org>
2019-05-13 21:54:19 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:54:19 up 22:18,  1 user,  load average: 0.63, 0.30, 0.30 b38d07f7ef0106b6a1a0f1c5f6fc4767d481ebfb

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -22,7 +22,6 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  Log("------------------------");
   //_exit(SYS_open);
   return _syscall_(SYS_open, (uintptr_t)path, (uintptr_t)flags, (uintptr_t)mode);
 }
@@ -46,19 +45,16 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  Log("------------------------");
   //_exit(SYS_read);
   return _syscall_(SYS_read, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
 }
 
 int _close(int fd) {
-  Log("------------------------");
   return _syscall_(SYS_close, (uintptr_t)fd, 0, 0);
   //_exit(SYS_close);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  Log("------------------------");
   //_exit(SYS_lseek);
   return _syscall_(SYS_lseek, (uintptr_t)fd, (uintptr_t)offset, (uintptr_t)whence);
 }
