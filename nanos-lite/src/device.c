48a7c88336e1a5e7eead9f47bd3a5f8b32e29afb
tracer-ics2017 <tracer@njuics.org>
2019-05-14 16:21:50 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:21:50 up 1 day, 38 min,  1 user,  load average: 1.31, 0.86, 0.48 b12c6cb62c015a676cd88258afbda5aed67a048a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -36,7 +36,7 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 void fb_write(const void *buf, off_t offset, size_t len) {
 	int row = (offset/4)/_screen.width;
 	int col = (offset/4)%_screen.width;
-	_draw_rect(buf,col,row,len,1);
+	_draw_rect(buf,row,col,len/4,1);
 }
 
 void init_device() {
