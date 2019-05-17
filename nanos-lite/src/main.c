82d5c3f78d1bd83957286ab295de0459baee3cab
tracer-ics2017 <tracer@njuics.org>
2019-05-17 10:11:55 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  10:11:55 up 1 day,  5:21,  1 user,  load average: 0.78, 0.32, 0.17 edb94d584717de703f0d037b62d8fc43a3578431

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL,"/bin/init");
+  uint32_t entry = loader(NULL,"/bin/pal");
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
