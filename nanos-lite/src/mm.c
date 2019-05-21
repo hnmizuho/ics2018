e27014898c74fe61a888e883cc2378de2306937c
tracer-ics2017 <tracer@njuics.org>
2019-05-21 16:12:09 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:12:09 up 1 day, 15:31,  1 user,  load average: 0.24, 0.34, 0.29 4c91f553e769dc8aadcacb13d30e70ed16d2899

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -22,12 +22,13 @@ int mm_brk(uint32_t new_brk) {
 		if (new_brk > current->max_brk) {
 	        	// TODO: map memory region [current->max_brk, new_brk)
 	        	// into address space current->as
-	        	uintptr_t va = current->max_brk;
+			int va = current->max_brk;
+	        	//uintptr_t va = (current->max_brk+0xfff) & ~0xfff;
 	        	while(va < new_brk) {
                 		_map(&current->as, (void *)va, (void *)new_page());
 				va += PGSIZE;
 			}
-			current->max_brk = new_brk;
+			current->max_brk = new_brk; // =va ?
 		}
 	current->cur_brk = new_brk;
 	}	
