02a7ddd665f1cac20c195402eceb9769643f637e
tracer-ics2017 <tracer@njuics.org>
2019-05-21 20:58:26 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:58:26 up 1 day, 20:17,  1 user,  load average: 0.06, 0.11, 0.17 d867dff89730994f7f460fb622609f2871f8284e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ extern _RegSet* schedule(_RegSet *prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
-    case _EVENT_SYSCALL:return do_syscall(r); //return null 或 break
+    case _EVENT_SYSCALL:return schedule(do_syscall(r));//return do_syscall(r); //return null 或 break
     case _EVENT_TRAP:return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
   //uint32_t entry = loader(NULL,"/bin/pal");
   //((void (*)(void))entry)();
   load_prog("/bin/pal");
-
+  load_prog("/bin/hello");
   _trap();
 
   panic("Should not reach here");
