84f0ac13422ba4fd634cb5e505c01fb7f0478805
tracer-ics2017 <tracer@njuics.org>
2019-05-21 21:31:24 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:31:24 up 1 day, 20:50,  1 user,  load average: 0.53, 0.36, 0.27 739ea1fa8b04e547ea134ae5293d5c5d35d3d867

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,19 +23,18 @@ void load_prog(const char *filename) {
 
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
 }
+
 int count = 0;
 _RegSet* schedule(_RegSet *prev) {
   //save the context pointer
   current->tf = prev;
 
-  //always select pcb[0] as the new process
   current = &pcb[0];
   count++;
-  if(count == 100){
+  if(count == 64){
   	count = 0;
   	current = &pcb[1];
   }
-  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   //TODO: switch to the new address space,
   //then return the new context
