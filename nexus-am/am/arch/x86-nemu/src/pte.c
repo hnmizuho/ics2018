f3b5b1264519c29b6e108e007454c2c96fd05ef3
tracer-ics2017 <tracer@njuics.org>
2019-05-21 14:59:22 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:59:22 up 1 day, 14:18,  1 user,  load average: 0.48, 0.41, 0.30 3ba665d81562d976b9c84570fc1ab97838aec106

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -66,20 +66,29 @@ void _switch(_Protect *p) {
 }
 
 void _map(_Protect *p, void *va, void *pa) {
-	PDE *pde, *pgdir = p->ptr;
-	PTE *pgtab;
-
-    pde = &pgdir[PDX(va)];
-	if (*pde & PTE_P) {
-		pgtab = (PTE *)PTE_ADDR(*pde);
+//printf("va: 0x%-8x pa: 0x%-8x\n", (uintptr_t)va, (uintptr_t)pa);
+	int pde_index = PDX(va);
+	//printf("pde_index: %d\n", pde_index);
+	int pte_index = PTX(va);
+	//printf("pte_idex: %d\n", pte_index);
+	PDE * updir = (PDE *)(p->ptr);
+	PTE * uptab;
+	if ((updir[pde_index] & 0x1) == 0){
+		uptab = (PTE *)(void *)(palloc_f());
+		//printf("uptab: 0x%-8x\n", (uintptr_t)uptab);
+		updir[pde_index] = (uintptr_t)uptab | PTE_P;
+		//printf("updir[%d] = 0x%-8x\n", pde_index, (uintptr_t)(updir[pde_index]));
 	} else {
-		pgtab = (PTE *)palloc_f();
-		for (int i = 0; i < NR_PTE; i ++) {
-		    pgtab[i] = 0;
-		}
-		*pde = PTE_ADDR(pgtab) | PTE_P;
+		uptab = (PTE *)(void *)((uintptr_t)updir[pde_index] & 0xfffff000);
+		//printf("uptab: 0x%-8x\n", (uintptr_t)uptab);
+		//printf("updir[%d] = 0x%-8x\n", pde_index, (uintptr_t)(updir[pde_index]));
 	}
-	pgtab[PTX(va)] = PTE_ADDR(pa) | PTE_P;
+
+	PTE pte = ((uintptr_t)pa & 0xfffff000) | PTE_P;
+	//printf("pte: 0x%-8x\n", pte);
+	uptab[pte_index] = pte;
+	//printf("uptab[%d] = 0x%-8x\n", pte_index-1, (uintptr_t)(uptab[pte_index-1]));
+	//printf("uptab[%d] = 0x%-8x\n", pte_index, (uintptr_t)(uptab[pte_index]));
 }
 
 void _unmap(_Protect *p, void *va) {
