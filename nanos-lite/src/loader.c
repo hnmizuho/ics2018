0b791c12f8eb0ce204a1033461488bc70bfebec4
tracer-ics2017 <tracer@njuics.org>
2019-05-21 15:10:11 +0800

    >  compile 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:10:11 up 1 day, 14:29,  1 user,  load average: 0.27, 0.29, 0.27 e39bdda8e7feb699dec2db4b53d76d45b518d15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,28 +12,16 @@ ssize_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  /*size_t size = get_ramdisk_size();
-  void * buff = NULL;
-  ramdisk_read(buff,0,size); 
-  memcpy(DEFAULT_ENTRY,buff,size); //之前误用memset
-  //后来才想起来，ramdisk_read已经memcpy了，上一句无用功
-  return (uintptr_t)DEFAULT_ENTRY;*/
-
   int fd = fs_open(filename, 0, 0);
-  size_t bytes = fs_filesz(fd);
-
-  Log("Load [%d] %s with size: %d", fd, filename, bytes);
-
-  void *pa,*va = DEFAULT_ENTRY;
-  while(bytes>0){
-  	pa = new_page(); //申请空闲物理页
-  	_map(as, va, pa);//该物理页映射到用户程序虚拟地址空间
-  	fs_read(fd, pa, PGSIZE);  //读一页文件到该物理页
-
-  	va += PGSIZE;
-  	bytes -= PGSIZE;
+  int filesize = fs_filesz(fd);
+  void *pa;
+  void *va = DEFAULT_ENTRY;
+  while(filesize > 0) {
+	  pa = new_page();
+	  _map(as, va, pa);
+	  va += PGSIZE;
+	  fs_read(fd, pa, PGSIZE);  
+	  filesize -= PGSIZE;
   }
-  //fs_read(fd,DEFAULT_ENTRY,bytes);
-  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
