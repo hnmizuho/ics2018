db3dbc2c557d9e8ccb5f212d13eea9f3834d309f
tracer-ics2017 <tracer@njuics.org>
2019-05-21 19:55:29 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:55:29 up 1 day, 19:14,  1 user,  load average: 0.20, 0.21, 0.18 d8a0d20ebf167bdc9a03f93a150c3e50b93f209e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:return do_syscall(r); //return null 或 break
-    //case _EVENT_TRAP:return schedule(r);
+    case _EVENT_TRAP:return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
