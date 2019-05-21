4732652ab71bfe56ed9d62f334019a9949253569
tracer-ics2017 <tracer@njuics.org>
2019-05-21 20:17:04 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:17:04 up 1 day, 19:36,  1 user,  load average: 0.35, 0.28, 0.21 24e946b37217893458b2a787e293971c7f7741a6

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,8 @@ int main() {
   //uint32_t entry = loader(NULL,"/bin/pal");
   //((void (*)(void))entry)();
   load_prog("/bin/pal");
-  //_trap();
+
+  _trap();
 
   panic("Should not reach here");
 }
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,9 +13,9 @@ void load_prog(const char *filename) {
   _protect(&pcb[i].as);
   uintptr_t entry = loader(&pcb[i].as, filename);
   // TODO: remove the following three lines after you have implemented _umake()
-  _switch(&pcb[i].as);
-  current = &pcb[i];
-  ((void (*)(void))entry)();
+  //_switch(&pcb[i].as);
+  //current = &pcb[i];
+  //((void (*)(void))entry)();
 
   _Area stack;
   stack.start = pcb[i].stack;
