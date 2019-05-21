31bedd601df146cf13af1199a6ef90d5451622ae
tracer-ics2017 <tracer@njuics.org>
2019-05-21 15:03:34 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:03:34 up 1 day, 14:22,  1 user,  load average: 0.20, 0.29, 0.27 8c6cf284a46b02545586c2377f52c899727c975

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,10 +1,10 @@
 #include "common.h"
-#include "memory.h"
 
 #define DEFAULT_ENTRY ((void *)0x08048000)
 //extern void ramdisk_read(void *buf, off_t offset, size_t len);
 //extern size_t get_ramdisk_size();
 extern void _map(_Protect *p, void *va, void *pa);
+extern void* new_page(void);
 
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_filesz(int fd);
@@ -26,7 +26,6 @@ uintptr_t loader(_Protect *as, const char *filename) {
 
   void *pa,*va = DEFAULT_ENTRY;
   while(bytes>0){
-Log("!");
   	pa = new_page(); //申请空闲物理页
   	_map(as, va, pa);//该物理页映射到用户程序虚拟地址空间
   	fs_read(fd, pa, PGSIZE);  //读一页文件到该物理页
