a224f055fb1e8040c0424ab66374ab84819ef0d9
tracer-ics2017 <tracer@njuics.org>
2019-05-22 17:31:54 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:31:54 up 1 day, 23:10,  1 user,  load average: 1.64, 1.11, 0.68 d4128483cbd8e7bac501e7b7f4ece686da77c930

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,7 +34,6 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-	Log("%s",dispinfo);
 	memcpy(buf,dispinfo+offset,len);
 }
 
