c45a999c221c83afdc9bd6136429ded3ab91a87b
tracer-ics2017 <tracer@njuics.org>
2019-05-22 17:12:13 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:12:13 up 1 day, 22:51,  1 user,  load average: 0.59, 0.42, 0.43 c7c22c65674debf0af1a62d6f24e6ac8c11553f5

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -49,5 +49,5 @@ void init_device() {
   _ioe_init();
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",40,30);
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d\n",400,300);
 }
