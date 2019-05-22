c3f6b90567493f05f830cfa58c2a0b2858695a44
tracer-ics2017 <tracer@njuics.org>
2019-05-22 17:28:33 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:28:33 up 1 day, 23:07,  1 user,  load average: 0.44, 0.41, 0.38 f3ceb2f5fc2615cd2f6d67af4c6be36ed6c5cc12

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -138,7 +138,8 @@ static void get_display_info() {
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
   fclose(dispinfo);
-  printf("---------------------------------");
+  screen_w = 400;
+  screen_h = 300;
   assert(screen_w > 0 && screen_h > 0);
 }
 
