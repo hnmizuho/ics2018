4ef7bf53b61c4343e2bd1fd4b75a1f749e0276f2
tracer-ics2017 <tracer@njuics.org>
2019-05-22 16:02:23 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:02:23 up 1 day, 21:41,  1 user,  load average: 0.18, 0.41, 0.35 b08ffa41133a62cd354e04c113fb568e115a7d7a

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,8 +34,8 @@ int main() {
   //((void (*)(void))entry)();
 
   //load_prog("/bin/pal");
-  load_prog("/bin/hello");
-    //load_prog("/bin/litenes");
+    load_prog("/bin/hello");
+    load_prog("/bin/litenes");
 
 
   _trap();
