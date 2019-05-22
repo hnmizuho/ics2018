18acc7261bf062d6c6048b67a14f80896eb94e9a
tracer-ics2017 <tracer@njuics.org>
2019-05-22 15:56:07 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:56:07 up 1 day, 21:35,  1 user,  load average: 1.32, 0.69, 0.36 b398bbda72c64b35af2966b0c8a8b79af3309f10

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:return do_syscall(r);//do_syscall(r);return schedule(r);
     case _EVENT_TRAP:return schedule(r);
-    case _EVENT_IRQ_TIME:Log("_EVENT_IRQ_TIME!!");return schedule(r);
+    case _EVENT_IRQ_TIME:/*Log("_EVENT_IRQ_TIME!!");*/return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
