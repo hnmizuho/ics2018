d8fad194e1ef88b81f57b82fb1ecd24da453f148
tracer-ics2017 <tracer@njuics.org>
2019-05-22 17:09:15 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:09:15 up 1 day, 22:48,  1 user,  load average: 0.27, 0.39, 0.43 846d5a8c534056d78e85c13ee56bcc1558bc3f7

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -45,7 +45,6 @@ int fs_open(const char *pathname, int flags, int mode) {
 	for (i = 0; i < NR_FILES; i++) {
 		//printf("file name: %s\n", file_table[i].name);
 		if (strcmp(file_table[i].name, pathname) == 0) {
-	Log("fuck: %d", i);
 			return i;
 		}
 	}
