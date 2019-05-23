34a96a35954b02b03931d9e15c2995e3869c3f86
tracer-ics2017 <tracer@njuics.org>
2019-05-23 22:55:36 +0800

    >  run 1611276 hnmizuho Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:55:36 up 2 days,  4:20,  1 user,  load average: 0.24, 0.14, 0.10 1c9f81e314d65eb8687c7c6bb0f4cc6f2d20860

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,10 +64,10 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
 			len = events_read((void *)buf, len);
 			break;
 		case FD_DISPINFO:
-			if (file_table[fd].open_offset >= file_table[fd].size) // == ??
+			if (file_table[fd].open_offset >= fs_size) // == ??
 				return 0;
-			if (file_table[fd].open_offset + len > file_table[fd].size) //超出部分舍弃
-				len = file_table[fd].size - file_table[fd].open_offset;
+			if (file_table[fd].open_offset + len > fs_size) //超出部分舍弃
+				len = fs_size - file_table[fd].open_offset;
 			dispinfo_read(buf, file_table[fd].open_offset, len);
 			file_table[fd].open_offset += len;	
 			break;
